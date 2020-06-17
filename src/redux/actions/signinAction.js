76896a09f55c0407b776c4d974ecb4b89b8b707d
Diva Yurian DWika <divayuriandwika@gmail.com>
2020-06-17 17:00:26 +0700

    edit signinaction

--- src/redux/actions/signinAction.js
+++ src/redux/actions/signinAction.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
-// import {useHistory} from 'react-router-dom'
 
 const apiUrl = process.env.REACT_APP_API_URL;
-// const history = useHistory()
+
 
 export const loginUser = (values, history) => {
 	return (dispatch) => {
@@ -13,17 +12,6 @@ export const loginUser = (values, history) => {
 				localStorage.setItem('role', response.data.role);
 				localStorage.setItem('message', response.data.message);
 				dispatch(newLogin(response.data.role, history))
-				// history.push('/manager')
-				// if(response.data !== 'undefined'){
-				// 	if (response.data.role === 'manager'){
-				// 		history.push('/manager')
-				// 	}
-
-				// }
-				// localStorage.setItem('refToken', response.data.refreshToken);
-				// localStorage.setItem('isLoggedIn', true);
-                // dispatch(currentUser(response.data.token));
-                // console.log(response.data)
                 
 			})
 			.catch((error) => {
